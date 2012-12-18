8f67e9dc53d6dee798bff131c045c69f3a95c727
Zack Hovatter <zackhovatter@gmail.com>
2012-12-18 13:20:18 -0600

    Use sandbox API key for development env

--- lib/hotels_combined.rb
+++ lib/hotels_combined.rb
@@ -5,8 +5,9 @@ require "hotels_combined/version"
 require "hotels_combined/configuration"
 
 module HotelsCombined
-  SANDBOX_URL = "http://sandbox.hotelscombined.com/API/Search.svc/pox/"
   LIVE_URL = "http://www.hotelscombined.com/API/Search.svc/pox/"
+  SANDBOX_URL = "http://sandbox.hotelscombined.com/API/Search.svc/pox/"
+  SANDBOX_API_KEY = "F7538EB2-2B63-4AE1-8C43-2FAD2D83EACB"
 
   def self.base_url
     if HotelsCombined.configuration.env == "production"
--- lib/hotels_combined/configuration.rb
+++ lib/hotels_combined/configuration.rb
@@ -15,5 +15,10 @@ module HotelsCombined
       @api_key = "CHANGE ME"
       @env = "development"
     end
+
+    def api_key
+      return HotelsCombined::SANDBOX_API_KEY unless @env != "production"
+      @api_key
+    end
   end
 end
